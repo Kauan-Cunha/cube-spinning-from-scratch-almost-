d756ed3f2281a487627ab9007633fc9d8f6bbe14
Kauan Cunha <kauancunha6@gmail.com>
2026-08-27 21:17:59 -0300

    Implement abstract shape class and cube construction with rotation methods

--- easy.py
+++ easy.py
@@ -2,6 +2,7 @@ import pygame
 from typing import Tuple, List
 import time
 import math
+from abc import ABC, abstractmethod
 
 WIDTH = 900
 HEIGHT = WIDTH
@@ -22,6 +23,53 @@ class Ponto():
     def setZ(self, new_z: float):
         self.z = new_z
 
+class Forma(ABC):
+    def __init__(self):
+        """
+        Classe abstrata de uma forma.
+        self.vtxs: lista de pontos dos vértices
+        self.conn: ordem de conexão das arestas da lista self.vtxs
+        """
+        self.vtxs: List[Ponto] = []
+        self.conn: List[Tuple[int, int]] = []
+        self.linePoints: List[Ponto] = []
+
+    @abstractmethod
+    def construct(self):
+        pass
+
+    def drawLines(self):
+        for edge in self.conn:
+            self.linePoints.extend(drawLine(self.vtxs[edge[0]], self.vtxs[edge[1]]))
+
+    def rotateX(self, teta: float):
+        for ponto in self.vtxs:
+            rotateX(ponto, teta)
+        for ponto in self.linePoints:
+            rotateX(ponto, teta)
+    def rotateY(self, teta: float):
+        for ponto in self.vtxs:
+            rotateY(ponto, teta)
+        for ponto in self.linePoints:
+            rotateY(ponto, teta)
+    def rotateZ(self, teta: float):
+        for ponto in self.vtxs:
+            rotateZ(ponto, teta)
+        for ponto in self.linePoints:
+            rotateZ(ponto, teta)
+
+class Cube(Forma):
+    def __init__(self):
+        super().__init__()
+
+    def construct(self, side: float):
+        half = side/2
+        self.vtxs.extend([Ponto(-half, -half, -half), Ponto(half, -half, -half), Ponto(half, half, -half),Ponto(-half, half, -half),
+                         Ponto(-half, -half, half), Ponto(half, -half, half), Ponto(half, half, half), Ponto(-half, half, half)])
+
+        self.conn.extend([(0, 1), (1, 2), (2, 3), (3, 0), (4, 5), (5, 6), (6, 7), (7, 4),
+                  (0, 4), (1, 5), (2,6), (3, 7)])
+
 def centralize(pontos: Ponto, width: int, height:int) -> Ponto:
     res = []
     for ponto in pontos:
@@ -30,22 +78,22 @@ def centralize(pontos: Ponto, width: int, height:int) -> Ponto:
     return res
 
 def rotateZ(ponto: Ponto, teta: float):
-    currX = ponto.x - 450
-    currY = ponto.y - 450
-    ponto.setX((currX*math.cos(teta) - currY*math.sin(teta))+ 450)
-    ponto.setY((currX*math.sin(teta) + currY*math.cos(teta))+ 450)
+    currX = ponto.x
+    currY = ponto.y
+    ponto.setX((currX*math.cos(teta) - currY*math.sin(teta)))
+    ponto.setY((currX*math.sin(teta) + currY*math.cos(teta)))
 
 def rotateX(ponto: Ponto, teta: float):
-    currZ = ponto.z -200
-    currY = ponto.y - 450
-    ponto.setY((currY*math.cos(teta) - currZ*math.sin(teta)) + 450)
-    ponto.setZ((currY*math.sin(teta) + currZ*math.cos(teta)) + 200)
+    currZ = ponto.z 
+    currY = ponto.y 
+    ponto.setY((currY*math.cos(teta) - currZ*math.sin(teta)))
+    ponto.setZ((currY*math.sin(teta) + currZ*math.cos(teta)))
 
 def rotateY(ponto: Ponto, teta: float):
-    currX = ponto.x - 450
-    currZ = ponto.z - 200
-    ponto.setX((currX*math.cos(teta) + currZ*math.sin(teta))+ 450)
-    ponto.setZ((currZ*math.cos(teta)-currX*math.sin(teta))+ 200)
+    currX = ponto.x
+    currZ = ponto.z
+    ponto.setX((currX*math.cos(teta) + currZ*math.sin(teta)))
+    ponto.setZ((currZ*math.cos(teta)-currX*math.sin(teta)))
 
 def drawLine(a: Ponto, b: Ponto, resolution: int = 100) -> List[Ponto]:
     """Return points that draw lines in image"""
@@ -53,44 +101,40 @@ def drawLine(a: Ponto, b: Ponto, resolution: int = 100) -> List[Ponto]:
     res = []
     #r : (x, y, z) = (x0, y0, z0) + t(x1 - x0, y1 - y0, z1 - z0)
     while t < 1:
-        print
         res.append(Ponto(a.x + t*(b.x - a.x), a.y + t*(b.y - a.y), a.z + t*(b.z - a.z)))
         t += step
 
     return res
 
+def drawForma(screen: pygame.Surface, forma: Forma, showVtx:bool = True, showEdge:bool = True):
+    #desenha vtx
+    if showVtx:
+        for ponto in forma.vtxs:
+            pygame.draw.circle(screen, "red", (ponto.x + WIDTH//2, ponto.y + HEIGHT//2), (VTX_RADIUS*DIST)//(2*(ponto.z + 500 + DIST)))
+
+    # desenha aresta
+    if showEdge:
+        for ponto in forma.linePoints:
+            pygame.draw.circle(screen, "red", (ponto.x + WIDTH//2, ponto.y + HEIGHT//2), (VTX_RADIUS*DIST)//(2*(ponto.z + 500 + DIST)))
+
 def main():
     #setup
     pygame.init()
     screen = pygame.display.set_mode((WIDTH, HEIGHT))
-    
-    vtxs = [Ponto(-200, 0, 100), Ponto(0, -200, 100), Ponto(200, 0, 100), Ponto(0, 200, 100),
-             Ponto(-200, 0, 400), Ponto(0, -200, 400), Ponto(200, 0, 400), Ponto(0, 200, 400)]
 
-    cVtxs =centralize(vtxs, WIDTH, HEIGHT)
-
-    cVtxs.extend(drawLine(cVtxs[0], cVtxs[1]))
-    cVtxs.extend(drawLine(cVtxs[1], cVtxs[2]))
-    cVtxs.extend(drawLine(cVtxs[2], cVtxs[3]))
-    cVtxs.extend(drawLine(cVtxs[3], cVtxs[0]))
-    cVtxs.extend(drawLine(cVtxs[4], cVtxs[5]))
-    cVtxs.extend(drawLine(cVtxs[5], cVtxs[6]))
-    cVtxs.extend(drawLine(cVtxs[6], cVtxs[7]))
-    cVtxs.extend(drawLine(cVtxs[7], cVtxs[4]))
-    cVtxs.extend(drawLine(cVtxs[0], cVtxs[4]))
-    cVtxs.extend(drawLine(cVtxs[1], cVtxs[5]))
-    cVtxs.extend(drawLine(cVtxs[2], cVtxs[6]))
-    cVtxs.extend(drawLine(cVtxs[3], cVtxs[7]))
+    #inicialize the Cube
+    forma: Forma = Cube()
+    forma.construct(300)
+    forma.drawLines()
 
     #draw
     running = True
     while running:
         screen.fill("purple")
-        for ponto in cVtxs:
-            rotateX(ponto, 0.005)
-            # rotateY(ponto, 0.005)
-            rotateZ(ponto, 0.005)
-            pygame.draw.circle(screen, "red", (ponto.x, ponto.y), (VTX_RADIUS*DIST)//(2*(ponto.z + DIST)))
+
+
+        drawForma(screen, forma)
+        forma.rotateY(0.005)
 
         time.sleep(0.001)
         pygame.display.flip()
